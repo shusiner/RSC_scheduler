53fb003b82e59407e878499fd96450abd13c26fb
shusiner <quantum.plato@gmail.com>
2018-08-13 00:43:22 +0800

    Added files and changes required for deployment to heroku

--- rsc2/settings.py
+++ rsc2/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
