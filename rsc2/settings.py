1d0bb068e5a209db0adbe826b55250f739ca544b
shusiner <quantum.plato@gmail.com>
2018-08-13 00:58:58 +0800

    Added files and changes required for deployment to heroku

--- rsc2/settings.py
+++ rsc2/settings.py
@@ -38,7 +38,7 @@ DATABASES = {
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sleepy-retreat-41065.herokuapp.com','127.0.0.1']
 
 
 # Application definition
