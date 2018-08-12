a80df69571ae797d47a33db4c8cb9112ce1c1747
shusiner <quantum.plato@gmail.com>
2018-08-13 01:05:05 +0800

    Added files and changes required for deployment to heroku

--- scheduler/models.py
+++ scheduler/models.py
@@ -134,7 +134,7 @@ class Site(models.Model):
 
 class Schedule(models.Model):
     guard = models.ForeignKey('Guard', on_delete=models.CASCADE)
-    #date = models.DateField()
+    dates = models.DateField()
     slug = models.SlugField(null=True)
     
     is_day = models.BooleanField(default=False)
